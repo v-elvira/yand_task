56e258ffebb4d532fff6273c7194a25941c51d4a
v-elvira <96012499+v-elvira@users.noreply.github.com>
2022-09-15 22:19:29 +0300

    Update models.py

--- open_api/models.py
+++ open_api/models.py
@@ -3,14 +3,12 @@ from django.db import models
 class Folder(models.Model):
 	"""Модель папок"""
 	id = models.CharField(primary_key=True, max_length=255)
-	# url = models.CharField(max_length=255, blank=False)
 	date = models.DateTimeField()
 	parentId = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
-	#size = models.IntegerField()
 
 
 class File(models.Model):
-	"""Модель папок"""
+	"""Модель файлов"""
 	id = models.CharField(primary_key=True, max_length=255)
 	url = models.CharField(blank=False, max_length=255)
 	date = models.DateTimeField()
